c4fa9cb58223caf318b767a2d91952f6da3c3980
lucas <lucws2002@gmail.com>
2024-05-31 20:57:56 -0400

    feat: added postadmin in admin module and comment admin in admin module

--- siteDjango/blog/admin.py
+++ siteDjango/blog/admin.py
@@ -1,2 +1,17 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'status', 'created_at', 'updated_at')
+    list_filter = ('status','title')
+    search_fields = ["title", "content", "created_at"]
+    prepopulated_fields = {'slug': ('title', 'content')}
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'likes', 'shares', 'created_at', 'updated_at')
+    list_filter = ('content','likes', 'shares')
+    search_fields = ["content", "created_at", "likes", "shares"]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- siteDjango/blog/models/comment.py
+++ siteDjango/blog/models/comment.py
@@ -5,8 +5,8 @@ from .post import Post
 class Comment(models.Model):
     content = models.CharField(max_length=200)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    likes: models.IntegerField(default=0)
-    shares: models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    shares = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_comments')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
